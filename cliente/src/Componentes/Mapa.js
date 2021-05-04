b3f08f20ef8480bc94371a1b2118fa43e94f899e
Jonathan <jon.hiidalgo92@gmail.com>
2021-05-04 17:24:19 -0600

    graficas
    se agregaron plantillas para graficass

--- cliente/src/Componentes/Mapa.js
+++ cliente/src/Componentes/Mapa.js
@@ -1,14 +1,13 @@
 import React from 'react'
+import Pastel from '../Estructuras/Pastel';
+import DesNav from '../Navegacion/DesNav';
+import Desplegable from '../Navegacion/Desplegable';
 
 export default class Mapa extends React.Component{
 
     render(){
-
-
-
-
         return(
-            <div> soy mapa interactivo </div>
+            <DesNav/>
         );
     }
 
