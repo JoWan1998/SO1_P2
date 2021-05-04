e8954616ed422a3e5c50bbdd839c68d6b20f009a
Jonathan <jon.hiidalgo92@gmail.com>
2021-05-04 00:59:46 -0600

    server redis, appweb, redisBD
    Se agrego app web, servidor de redis, y  el servidor de BD

---
+++ cliente/src/Componentes/Mapa.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default class Mapa extends React.Component{
+
+    render(){
+
+
+
+
+        return(
+            <div> soy mapa interactivo </div>
+        );
+    }
+
+
+
+}
